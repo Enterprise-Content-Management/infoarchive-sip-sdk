9e7e2d503accbd4f0e8bf3e66dd0b22138c172f5
Remon Sinnema <remon.sinnema@emc.com>
2016-06-16 14:27:55 +0200

    Publish core jars to Central Repository

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
-  gradlew uploadArchives
-else
-  echo TRAVIS_TAG=$TRAVIS_TAG
-  echo TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST
-  echo TRAVIS_SECURE_ENV_VARS=$TRAVIS_SECURE_ENV_VARS
-fi
