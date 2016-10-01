76560afe5cbfbe08e2a07fb14213bd74ce99c79d
Ray Sinnema <rssinnema@yahoo.com>
2016-10-01 18:44:15 +0200

    Add Hello, SIP sample

---
+++ samples/hello-sip/src/main/java/com/emc/ia/sip/sample/hello/Greeting.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016 EMC Corporation. All Rights Reserved.
+ */
+package com.emc.ia.sip.sample.hello;
+
+
+/**
+ *  Our domain object that holds the data to be archived.
+ */
+public class Greeting {
+
+  private final String message;
+
+  public Greeting(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+}
