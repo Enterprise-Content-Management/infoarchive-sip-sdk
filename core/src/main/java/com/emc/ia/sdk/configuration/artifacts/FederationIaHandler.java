e62b113dd16c55cac167737df6f9d2f5b1f70541
StaticGame <sinisterint@gmail.com>
2016-12-05 17:29:44 +0300

    WIP Declarative application configuration: added tests for federation installer

--- core/src/main/java/com/emc/ia/sdk/configuration/artifacts/FederationIaHandler.java
+++ core/src/main/java/com/emc/ia/sdk/configuration/artifacts/FederationIaHandler.java
@@ -5,6 +5,7 @@ package com.emc.ia.sdk.configuration.artifacts;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Objects;
 
 import com.emc.ia.sdk.configuration.ArtifactExtractor;
 import com.emc.ia.sdk.configuration.BaseIAArtifact;
@@ -23,7 +24,10 @@ public final class FederationIaHandler extends BaseIAArtifact {
   private final Federation federation;
 
   public FederationIaHandler(Federation source) {
-    this.federation = source;
+    this.federation = new Federation();
+    federation.setName(source.getName());
+    federation.setBootstrap(source.getBootstrap());
+    federation.setSuperUserPassword(source.getSuperUserPassword());
   }
 
   @Override
@@ -47,6 +51,25 @@ public final class FederationIaHandler extends BaseIAArtifact {
       }
     }
     cache.cacheOne(createdFederation);
+  }
+
+  public boolean equals(Object other) {
+    if (!(other instanceof FederationIaHandler)) {
+      return false;
+    }
+    FederationIaHandler handler = (FederationIaHandler)other;
+    return Objects.equals(federation.getName(), handler.federation.getName())
+        && Objects.equals(federation.getBootstrap(), handler.federation.getBootstrap())
+        && Objects.equals(federation.getSuperUserPassword(), handler.federation.getSuperUserPassword());
+  }
+
+  public int hashCode() {
+    int result = 7;
+    result = 31 * result + (federation.getName() == null ? 0 : federation.getName().hashCode());
+    result = 31 * result + (federation.getBootstrap() == null ? 0 : federation.getBootstrap().hashCode());
+    result = 31 * result
+                 + (federation.getSuperUserPassword() == null ? 0 : federation.getSuperUserPassword().hashCode());
+    return result;
   }
 
   private static final class FederationExtractor extends ArtifactExtractor {
---
+++ core/src/test/java/com/emc/ia/sdk/configuration/artifacts/FederationIaHandlerTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2016 EMC Corporation. All Rights Reserved.
+ */
+package com.emc.ia.sdk.configuration.artifacts;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import static com.emc.ia.sdk.sip.client.rest.InfoArchiveLinkRelations.LINK_ADD;
+import static com.emc.ia.sdk.support.rest.StandardLinkRelations.LINK_SELF;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.emc.ia.sdk.configuration.ArtifactExtractor;
+import com.emc.ia.sdk.configuration.BaseIAArtifact;
+import com.emc.ia.sdk.configuration.IACache;
+import com.emc.ia.sdk.sip.client.dto.Federation;
+import com.emc.ia.sdk.sip.client.dto.Federations;
+import com.emc.ia.sdk.sip.client.dto.Services;
+import com.emc.ia.sdk.support.rest.RestClient;
+
+public class FederationIaHandlerTest {
+
+  @Test
+  public void whenExtractingFederation() {
+    ArtifactExtractor federationExtractor = FederationIaHandler.extractor();
+    BaseIAArtifact federationArtifact = federationExtractor.extract(getAppRepresentation());
+    BaseIAArtifact expectedArtifact = getFederationSample();
+    assertEquals("Extracted artifact should be equal to expected", expectedArtifact, federationArtifact);
+  }
+
+  @Test
+  public void whenInstallingFederation() throws IOException {
+    BaseIAArtifact federationArtifact = getFederationSample();
+    IACache cache = new IACache();
+    cache.cacheOne(new Services());
+    RestClient client = mock(RestClient.class);
+    Federations federations = mock(Federations.class);
+    Federation resultFederation = new Federation();
+    resultFederation.setName("mainFederation");
+    when(client.follow(any(), any(), eq(Federations.class))).thenReturn(federations);
+    when(federations.byName("mainFederation")).thenReturn(null);
+    when(client.createCollectionItem(any(), any(), eq(LINK_ADD), eq(LINK_SELF))).thenReturn(resultFederation);
+
+    federationArtifact.install(client, cache);
+
+    verify(client).createCollectionItem(eq(federations), any(), eq(LINK_ADD), eq(LINK_SELF));
+  }
+
+  private FederationIaHandler getFederationSample() {
+    Federation federation = new Federation();
+    federation.setName("mainFederation");
+    federation.setSuperUserPassword("test");
+    federation.setBootstrap("xhive://127.0.0.1:2910");
+    return new FederationIaHandler(federation);
+  }
+
+  private Map<String, String> getAppRepresentation() {
+    Map<String, String> federationMap = new HashMap<>();
+    federationMap.put("name", "mainFederation");
+    federationMap.put("bootstrap", "xhive://127.0.0.1:2910");
+    federationMap.put("superUserPassword", "test");
+    return federationMap;
+  }
+
+}
