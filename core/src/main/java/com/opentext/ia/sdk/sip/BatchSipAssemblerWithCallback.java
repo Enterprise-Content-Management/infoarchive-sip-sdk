795f4407a088b85042d67350284d6e84f161bfcb
dsmithjo <dsmithjo@10.133.196.149>
2017-09-20 21:52:38 +0100

    Adds a callback feature to for BatchSipAssembler
    Enable a caller to know that a Sip has been closed.

---
+++ core/src/main/java/com/opentext/ia/sdk/sip/BatchSipAssemblerWithCallback.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016-2017 by OpenText Corporation. All Rights Reserved.
+ */
+package com.opentext.ia.sdk.sip;
+
+import java.io.File;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import com.opentext.ia.sdk.support.io.FileSupplier;
+
+/**
+ * {@linkplain BatchSipAssembler Assemble a batch of SIPs} with a callback when
+ * SIP is closed.
+ * <p>
+ * 
+ * @param <D>
+ *            The type of domain objects to assemble
+ */
+public class BatchSipAssemblerWithCallback<D> extends BatchSipAssembler<D> {
+	private Consumer<FileGenerationMetrics> callback;
+	private final Object lock = new Object();
+
+	public BatchSipAssemblerWithCallback(SipAssembler<D> assembler, SipSegmentationStrategy<D> segmentationStrategy,
+			Consumer<FileGenerationMetrics> callback) {
+		this(assembler, segmentationStrategy, FileSupplier.fromTemporaryDirectory(), callback);
+	}
+
+	public BatchSipAssemblerWithCallback(SipAssembler<D> assembler, SipSegmentationStrategy<D> segmentationStrategy,
+			File dir, Consumer<FileGenerationMetrics> callback) {
+		this(assembler, segmentationStrategy, FileSupplier.fromDirectory(dir), callback);
+	}
+
+	public BatchSipAssemblerWithCallback(SipAssembler<D> assembler, SipSegmentationStrategy<D> segmentationStrategy,
+			Supplier<File> fileSupplier, Consumer<FileGenerationMetrics> callback) {
+		super(assembler, segmentationStrategy, fileSupplier);
+		this.callback = callback;
+		setFinalSipInDss(true);
+	}
+
+	@Override
+	protected void sipEnded(FileGenerationMetrics metrics) {
+		synchronized (lock) {
+			super.sipEnded(metrics);
+		}
+		callback.accept(metrics);
+	}
+}
