8c75bd08f46e34f25f3a48a57c9b18fca9ca60ab
dsmithj0 <32142754+dsmithj0@users.noreply.github.com>
2017-09-22 10:11:01 +0100

    Update WhenAssemblingSipsInBatches.java
    Cleaned the imports a bit

--- core/src/test/java/com/opentext/ia/sdk/sip/WhenAssemblingSipsInBatches.java
+++ core/src/test/java/com/opentext/ia/sdk/sip/WhenAssemblingSipsInBatches.java
@@ -3,17 +3,9 @@
  */
 package com.opentext.ia.sdk.sip;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Matchers.isNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
 
 import java.io.File;
 import java.io.IOException;
