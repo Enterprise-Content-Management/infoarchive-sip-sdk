923ba5fefebf6626986490e00ea08b160471cd97
Ray Sinnema <rssinnema@yahoo.com>
2018-04-26 18:18:48 +0200

    Remove debug output

--- samples/sample-4-authenticate/src/main/java/com/opentext/ia/sdk/sample/authenticate/Authenticate.java
+++ samples/sample-4-authenticate/src/main/java/com/opentext/ia/sdk/sample/authenticate/Authenticate.java
@@ -37,9 +37,6 @@ public final class Authenticate {
   public static void main(String[] args) {
     HttpClient httpClient = new ApacheHttpClient();
     try {
-      System.out.println("Environment:");
-      System.getenv().forEach((key, value) -> System.out.printf("  %s=%s%n", key, value));
-      System.out.printf("%nSettings:%n");
       // Log in to the gateway with user name & password
       AuthenticationStrategy authentication = new JwtAuthentication(get(SETTING_USERNAME),
           get(SETTING_PASSWORD), new GatewayInfo(get(SETTING_GATEWAY_URL), get(SETTING_CLIENT_ID),
@@ -49,17 +46,15 @@ public final class Authenticate {
       Header header = authentication.issueAuthHeader();
 
       // Use this header in calls using the HTTP client
-      System.out.printf("%nHeader:%n%s%n", header);
+      System.out.printf("Header:%n%s%n", header);
     } finally {
       httpClient.close();
     }
   }
 
   private static String get(String name) {
-    String result = System.getProperty(name,
+    return System.getProperty(name,
         System.getenv().getOrDefault(name, DEFAULT_SETTINGS.getOrDefault(name, name)));
-    System.out.printf("  %s=%s%n", name, result);
-    return result;
   }
 
 }
