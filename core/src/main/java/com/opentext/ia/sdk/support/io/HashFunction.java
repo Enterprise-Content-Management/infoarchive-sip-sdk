f21209e0d9d1de97623c52567f8e4461375f6c6c
Vadim Podlesov <vadim@podlesov.info>
2020-02-28 10:27:49 +0300

    New sha3 hashes

--- core/src/main/java/com/opentext/ia/sdk/support/io/HashFunction.java
+++ core/src/main/java/com/opentext/ia/sdk/support/io/HashFunction.java
@@ -13,7 +13,11 @@ public enum HashFunction {
   SHA1("SHA-1"),
   SHA256("SHA-256"),
   SHA384("SHA-384"),
-  SHA512("SHA-512");
+  SHA512("SHA-512"),
+  SHA3_224("SHA3-224"),
+  SHA3_256("SHA3-256"),
+  SHA3_384("SHA3-384"),
+  SHA3_512("SHA3-512");
 
   private final String name;
 
