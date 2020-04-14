241327761aacc8ea9c6c51b85a6bf11755f18966
Vadim Podlesov <vadim@podlesov.info>
2020-04-14 10:25:09 +0300

    Add default.properties files explicitly. Do not scan for properties files. Old APi is deprecated but functional.

--- yaml/src/main/java/com/opentext/ia/yaml/configuration/zip/ZipConfiguration.java
+++ yaml/src/main/java/com/opentext/ia/yaml/configuration/zip/ZipConfiguration.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 import org.apache.commons.io.FilenameUtils;
