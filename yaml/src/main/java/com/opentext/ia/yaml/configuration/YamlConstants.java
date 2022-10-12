4b3324e1beb97bdb9b15db1648afc913169461e1
shenj <shenj@SHENJPF2ANFLL.opentext.net>
2022-10-12 15:44:04 -0400

    Rename a constant file to YamlConstants

--- yaml/src/main/java/com/opentext/ia/yaml/configuration/YamlConstants.java
+++ yaml/src/main/java/com/opentext/ia/yaml/configuration/YamlConstants.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public final class InfoarchiveYamlConstant {
+public final class YamlConstants {
 
     public static final List<String> RESOURCE_CONTAINER_PATHS = Collections.unmodifiableList(Arrays.asList("/.+/content(/\\d+)?",
         "/customPresentationConfiguration(s/[^/]+)?/htmlTemplate", "/database(s/[^/]+)?/metadata/\\d+",
@@ -16,7 +16,7 @@ public final class InfoarchiveYamlConstant {
 
     public static final String TEXT = "text";
 
-    private InfoarchiveYamlConstant() {
+    private YamlConstants() {
 
     }
 }
