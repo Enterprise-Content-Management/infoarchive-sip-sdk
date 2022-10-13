3bc27b890b85c95743c4277e4cf8bad978bcaa93
shenj <shenj@SHENJPF2ANFLL.opentext.net>
2022-10-12 22:38:12 -0400

    Cleanup commented-out testcases

--- core/src/test/java/com/opentext/ia/sdk/server/configuration/yaml/WhenConfiguringServerUsingYaml.java
+++ core/src/test/java/com/opentext/ia/sdk/server/configuration/yaml/WhenConfiguringServerUsingYaml.java
@@ -25,8 +25,6 @@ import com.opentext.ia.sdk.support.http.HttpClient;
 import com.opentext.ia.sdk.support.http.rest.Link;
 import com.opentext.ia.sdk.support.http.rest.RestClient;
 import com.opentext.ia.test.TestCase;
-//import com.opentext.ia.yaml.configuration.YamlConfiguration;
-
 
 @Disabled
 class WhenConfiguringServerUsingYaml extends TestCase implements InfoArchiveLinkRelations {
@@ -34,8 +32,6 @@ class WhenConfiguringServerUsingYaml extends TestCase implements InfoArchiveLink
   private final HttpClient httpClient = mock(HttpClient.class);
   private final ArchiveConnection connection = new ArchiveConnection();
   private final ApplicationConfigurer clientSideConfigurer = mock(ApplicationConfigurer.class);
-//  private final YamlBasedApplicationConfigurer configurer = new YamlBasedApplicationConfigurer(
-//      new YamlConfiguration("version: 1.0.0"), (yaml, conn) -> clientSideConfigurer);
 
   @BeforeEach
   public void init() throws IOException {
@@ -65,62 +61,5 @@ class WhenConfiguringServerUsingYaml extends TestCase implements InfoArchiveLink
     verify(clientSideConfigurer).configure(any());
     verify(httpClient, never()).put(anyString(), any(), any(), anyString());
   }
-
-//  @Test
-//  void shouldConvertYamlToProperties() throws IOException {
-//    Map<String, String> expected = loadProperties();
-//    Map<String, String> actual = yamlToProperties();
-//    assertEqual(normalizeWhitespace(expected), normalizeWhitespace(actual));
-//  }
-
-//  private Map<String, String> normalizeWhitespace(Map<String, String> map) {
-//    for (Entry<String, String> entry : map.entrySet()) {
-//      map.replace(entry.getKey(), entry.getValue().replaceAll("\\s+", " ").trim());
-//    }
-//    return map;
-//  }
-
-//  private Map<String, String> loadProperties() throws IOException {
-//    Properties properties = new Properties();
-//    try (InputStream input = WhenConfiguringServerUsingYaml.class.getResourceAsStream("/iaif/iaif.properties")) {
-//      properties.load(input);
-//    }
-//    Set<String> stringPropertyNames = properties.stringPropertyNames();
-//    Map<String, String> result = new HashMap<>(stringPropertyNames.size());
-//    for (String name : stringPropertyNames) {
-//      result.put(name, properties.getProperty(name));
-//    }
-//    return result;
-//  }
-
-//  private Map<String, String> yamlToProperties() throws IOException {
-//    try (InputStream input = WhenConfiguringServerUsingYaml.class.getResourceAsStream("/iaif/iaif.yaml")) {
-//      YamlConfiguration configuration = new YamlConfiguration(input, ResourceResolver.fromClasspath("/iaif"));
-//      return new YamlPropertiesMap(configuration.getMap().sort());
-//    }
-//  }
-//
-//  private void assertEqual(Map<String, String> expected, Map<String, String> actual) {
-//    expected.entrySet().stream()
-//        .sorted((e1, e2) -> e1.getKey().compareTo(e2.getKey()))
-//        .forEachOrdered(e -> assertEqual(e, actual));
-//  }
-
-//  private void assertEqual(Entry<String, String> expected, Map<String, String> actual) {
-//    if (!actual.containsKey(expected.getKey())) {
-//      fail(String.format("Missing key: %s%nGot:%n%s", expected.getKey(),
-//          actual.keySet().stream().collect(Collectors.joining(System.lineSeparator()))));
-//    }
-//    TestUtil.assertEquals(normalize(expected.getValue()), normalize(actual.get(expected.getKey())),
-//        expected.getKey());
-//  }
-
-//  private List<String> normalize(String value) {
-//    return Arrays.asList(value
-//        .replaceAll("(\\r|\\n)+\\s*", " ")
-//        .replaceAll("\\s+<", "<")
-//        .replaceAll(">\\s+", ">")
-//        .split(","));
-//  }
 
 }
--- samples/sample-3-ingest/src/main/java/com/opentext/ia/sdk/sample/ingest/YamlSipIngester.java
+++ samples/sample-3-ingest/src/main/java/com/opentext/ia/sdk/sample/ingest/YamlSipIngester.java
@@ -21,28 +21,10 @@ import java.io.IOException;
 //import com.opentext.ia.sdk.client.api.ArchiveConnection;
 //import com.opentext.ia.sdk.client.factory.ArchiveClients;
 import com.opentext.ia.sdk.server.configuration.InfoArchiveConnectionProperties;
-//import com.opentext.ia.sdk.server.configuration.PropertiesBasedArchiveConnection;
-
-//import com.opentext.ia.sdk.sip.ContentInfo;
-//import com.opentext.ia.sdk.sip.DigitalObject;
-//import com.opentext.ia.sdk.sip.DigitalObjectsExtraction;
-//import com.opentext.ia.sdk.sip.FileGenerationMetrics;
-//import com.opentext.ia.sdk.sip.FileGenerator;
-//import com.opentext.ia.sdk.sip.PackagingInformation;
-//import com.opentext.ia.sdk.sip.SipAssembler;
-//import com.opentext.ia.sdk.sip.XmlPdiAssembler;
-//import com.opentext.ia.sdk.support.io.FileSupplier;
-//import com.opentext.ia.sdk.support.io.StringStream;
 
 
 @SuppressWarnings("PMD")
 public class YamlSipIngester implements InfoArchiveConnectionProperties {
-
-//  private static final String SETTING_USERNAME = "username";
-//  private static final String SETTING_PASSWORD = "password";
-//  private static final String SETTING_GATEWAY_URL = "gateway.url";
-//  private static final String SETTING_CLIENT_ID = "client.id";
-//  private static final String SETTING_CLIENT_SECRET = "client.secret";
 
   public static void main(String[] args) {
     try {
@@ -59,92 +41,7 @@ public class YamlSipIngester implements InfoArchiveConnectionProperties {
   }
 
   private void run(String rootPath) throws IOException {
-    System.out.printf("%nThis test is not working anymoreore, can be removed - Sample 3: Assemble SIP from %s and ingest into InfoArchive%n", rootPath);
-
-    // Load the configuration
-//    YamlConfiguration configuration = new YamlConfiguration(new File(rootPath, "configuration.yml"));
-//
-//    // Tell InfoArchive where and how to archive the data
-//    URI entityUri = URI.create(configuration.getPdiSchemaName());
-//    String entityName = "animal";
-//    PackagingInformation prototype = PackagingInformation.builder().dss()
-//        .application(configuration.getApplicationName()).holding(configuration.getHoldingName()).producer("SIP-SDK")
-//        .entity(entityName).schema(entityUri.toString()).end().build();
-//
-//    // Define a mapping from our domain object to the PDI XML
-//    XmlPdiAssembler<File> pdiAssembler;
-//    try (InputStream schema = new StringStream(configuration.getPdiSchema())) {
-//      pdiAssembler = new XmlPdiAssembler<File>(entityUri, entityName, schema) {
-//
-//        @Override
-//        protected void doAdd(File value, Map<String, ContentInfo> ignored) {
-//          String name = value.getName();
-//          getBuilder().element("animal_name", name.substring(0, name.lastIndexOf('.'))).element("file_path",
-//              relativePath(value, rootPath));
-//        }
-//      };
-//    }
-//
-//    DigitalObjectsExtraction<File> contentAssembler =
-//        file -> Collections.singleton(DigitalObject.fromFile(relativePath(file, rootPath), file)).iterator();
-//
-//    // Assemble the SIP
-//    SipAssembler<File> sipAssembler = SipAssembler.forPdiAndContent(prototype, pdiAssembler, contentAssembler);
-//    FileGenerator<File> generator = new FileGenerator<>(sipAssembler, FileSupplier.fromTemporaryDirectory());
-//
-//    File f1 = new File(rootPath, "ape.dat");
-//    File f2 = new File(rootPath, "bear.dat");
-//    File f3 = new File(rootPath, "cobra.dat");
-//
-//    FileGenerationMetrics metrics = generator.generate(Arrays.asList(f1, f2, f3));
-//    File assembledSip = metrics.getFile();
-//
-//    // Get an ArchiveClient instance to interact with InfoArchive.
-//    // In this case, we start with a blank installation, and create a sample holding from scratch that will contain
-//    // the SIP we've just assembled.
-//    // Use ArchiveClients.usingAlreadyConfiguredServer() instead if you already configured the server with application,
-//    // holding, etc.
-//    ArchiveClient archiveClient = ArchiveClients
-//        .configuringApplicationUsing(new YamlBasedApplicationConfigurer(configuration), newArchiveConnection(rootPath));
-//
-//    // Ingest the SIP into InfoArchive
-//    try (InputStream sip = Files.newInputStream(assembledSip.toPath(), StandardOpenOption.READ)) {
-//      String aipId = archiveClient.ingestDirect(sip);
-//      System.out.println("  SIP ingested as AIP " + aipId);
-//    }
+    System.out.printf("%nThis test is not working anymore after yaml cleanup refactory, it can be removed - Sample 3: Assemble SIP from %s and ingest into InfoArchive%n", rootPath);
   }
-
-//  private String relativePath(File file, String rootPath) {
-//    return file.getAbsolutePath().substring(rootPath.length() + 1);
-//  }
-//
-//  private ArchiveConnection newArchiveConnection(String rootPath) throws IOException {
-//    try (InputStream connectionProperties =
-//        Files.newInputStream(Paths.get(rootPath, "connection.properties"), StandardOpenOption.READ)) {
-//      Properties properties = new Properties();
-//      properties.load(connectionProperties);
-//      override(properties);
-//      return new PropertiesBasedArchiveConnection(properties);
-//    }
-//  }
-
-//  private void override(Properties properties) {
-//    override(SERVER_AUTHENTICATION_GATEWAY, SETTING_GATEWAY_URL, properties);
-//    override(SERVER_AUTHENTICATION_USER, SETTING_USERNAME, properties);
-//    override(SERVER_AUTHENTICATION_PASSWORD, SETTING_PASSWORD, properties);
-//    override(SERVER_CLIENT_ID, SETTING_CLIENT_ID, properties);
-//    override(SERVER_CLIENT_SECRET, SETTING_CLIENT_SECRET, properties);
-//  }
-
-//  private void override(String key, String overrideProperty, Properties properties) {
-//    String override = get(overrideProperty);
-//    if (StringUtils.isNotBlank(override)) {
-//      properties.setProperty(key, override);
-//    }
-//  }
-
-//  private static String get(String name) {
-//    return System.getProperty(name, System.getenv().get(name));
-//  }
 
 }
